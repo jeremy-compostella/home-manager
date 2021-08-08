b202464e2d8ee340950d35efc8cd51dc77b74269
Jeremy Compostella <jeremy.compostella@gmail.com>
2021-08-08 16:31:21 -0700

    health_monitor.py: rework and fixes
    Signed-off-by: Jeremy Compostella <jeremy.compostella@gmail.com>

--- health_monitor.py
+++ health_monitor.py
@@ -26,27 +26,52 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import configparser
+import os
 import threading
 
+from bluetooth import *
 from consumer import *
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from datetime import datetime, timedelta
 from sensor import *
-from stat_sensor import MyEmail
+from statistics import median
+from tools import *
+
+def test_loop(name, msg, end_msg = None, sleep = 15, end = False, min_failure = 1):
+    def inner(fun):
+        def run_until_success(*args, **kwargs):
+            failed = 0
+            while True:
+                ret = fun(*args, **kwargs)
+                debug('%s:%s' % (name, ret))
+                if ret:
+                    if failed >= min_failure and end_msg:
+                        alert(end_msg)
+                    failed = 0
+                    if end:
+                        return ret
+                    time.sleep(sleep)
+                    continue
+                failed += 1
+                if failed == min_failure:
+                    alert(msg)
+                time.sleep(sleep)
+        return run_until_success
+    return inner
 
 reader = None
 class SensorReader:
     expiration = None
     usage = None
 
-    def __init__(self, vue):
-        self.vue = vue
+    def __init__(self, config):
+        self.vue = MyVue2(config)
         self._usage_lock = threading.Lock()
 
+    @test_loop('Emporia read', "Failed to access Emporia", "Emporia is back",
+               min_failure = 3, end = True)
     def __read(self):
         try:
-            return vue.read(scale=Scale.SECOND.value)
+            return self.vue.read(scale=Scale.SECOND.value)
         except:
             return False
 
@@ -54,83 +79,171 @@ class SensorReader:
         with self._usage_lock:
             if self.expiration and datetime.now() < self.expiration:
                 return self.usage
-            usage = test(lambda: self.__read(),
-                         "Emporia is unreachable", end_msg = "Emporia is back")
-            expiration = datetime.now() + timedelta(seconds=15)
+            self.usage = self.__read()
+            self.expiration = datetime.now() + timedelta(seconds=15)
         return self.usage
 
-def test(fun, msg, end_msg=None, timeout=90, sleep=15):
-    start = datetime.now()
-    while True:
-        ret = fun()
-        if ret:
-            if not start and end_msg:
-                mailer.sendMIMEText(end_msg)
-            return ret
-        if start and start + timedelta(seconds=timeout) < datetime.now():
-            mailer.sendMIMEText(msg)
-            start = None
-            time.sleep(sleep)
-
+# HVAC
+# ----
+# 1. When the Yellow wire is shunt by float T-switch the air handler
+#    stops running but the Heat Pump is still running
+@test_loop('Heat pump yellow',
+           "Heat pump running while air handler is stopped",
+           "Heat pump/air handler is back to normal",
+           min_failure = 3)
 def hvac_yellow():
-    print('%s' % __name__)
     usage = reader.read()
-    return usage['A/C'] > .1 and usage['air handler'] < .1,
+    return not (usage['A/C'] > .1 and usage['air handler'] < .1)
 
+# 2. When the Red wire is shunt by float T-switch, the air handler
+#    keeps running but the condensation and the heat pump do not. To
+#    avoid false positive, we use a large min_failure parameter as my
+#    HVAC is configured to run the air-handler for a few minutes after
+#    stopping the A/C.
+@test_loop('heat pump red',
+           "Air handler is running while Heat Pump is stopped",
+           "Heat pump/air handler is back to normal",
+           min_failure = 32)
 def hvac_red():
-    """HVAC: When the Yellow wire is shunt by float T-switch the air
-handler stops running but the Heat Pump is still running
-    """
-    print('%s' % __name__)
     usage = reader.read();
-    return usage['A/C'] > .1 and usage['air handler'] < .1
+    return not (usage['A/C'] < .1 and usage['air handler'] > .1)
+
+# 3. Test Ecobee access, in certain conditions, the entire HVAC system
+#    is shut down and the ecobee is not powered anymore
+@test_loop('ecobee alive',
+           "Ecobee has become inaccessible", "Ecobee is back",
+           sleep = 60, min_failure = 5)
+def sensor_is_running(device):
+    return device.read(cache=False)
+
+# 4. TODO: Install temperature sensors and monitor the delta T
+
+# Pool filtering system
+# ---------------------
+# 1. When the Pool Pump malfunction, it stops itself in less than 2
+#    minutes (see July 7th 2021 capacitor death)
+class PoolRanLongEnough(threading.Thread):
+    startRunning = None
+    def __init__(self, config, seconds):
+        super(PoolRanLongEnough, self).__init__()
+        self.seconds = seconds
+        self.sensor = config['sensors']
+
+    @test_loop('PoolRanLongEnough',
+               "Pool stopped after a few minutes")
+    def run(self):
+        usage = reader.read()
+        if not self.startRunning:
+            if usage[self.sensor] > .1:
+                self.startRunning = datetime.now()
+            return True
+        if usage[self.sensor] > .1:
+            return True
+        if datetime.now() > self.startRunning + timedelta(seconds=self.seconds):
+            self.startRunning = None
+            return True
+        return False
+
+# 2. TODO: Out of range power consumption (June 21 2021: pool filter
+#    head cracked)
+# 3. Pool filter is dirty
+class PoolFilterIsClean(threading.Thread):
+    def __init__(self, config):
+        super(PoolFilterIsClean, self).__init__()
+        self.sensor = config['sensors']
+
+    @test_loop('PoolFilterIsClean', 'Pool filter is dirty',
+               sleep=24 * 60 * 60)
+    def run(self):
+        reader = SensorLogReader(datetime.now() - timedelta(days=1))
+        power = []
+        for current in iter(reader):
+            if current[self.sensor] >= .4:
+                power.append(current[self.sensor])
+        return median(power) > 1.8
+
+ev_lock = threading.Lock()
+ev = None
+def ev_is_connected():
+    if not ev:
+        return False
+    with ev_lock:
+        return ev.isConnected()
+
+# Car
+# ----------------
+# 1. Car is in the garage but not plugged-in
+class CarIsPluggedIn(threading.Thread):
+    def __init__(self, config):
+        super(CarIsPluggedIn, self).__init__()
+        self.config = config
+        self.pluggedIn = self.inTheGarage = datetime(1970, 1, 1)
+
+    @test_loop('CarIsPluggedIn',
+               'The car is in the garage but is not plugged in',
+               end_msg = 'The car is now plugged in',
+               sleep = 15)
+    def run(self):
+        if ev_is_connected():
+            debug('Car is plugged in')
+            self.pluggedIn = datetime.now()
+            self.inTheGarage = datetime(1970, 1, 1)
+            return True
+        # Car is not connected, let's give it some time to exit the
+        # garage
+        now = datetime.now()
+        if now < self.pluggedIn + timedelta(minutes=10):
+            return True
+        # Car detection
+        if self.inTheGarage == datetime(1970, 1, 1):
+            if self.config['obd_MAC'] in discover_devices():
+                debug('Car entered the garage')
+                self.inTheGarage = now
+            return True
+        # Car has been detected for a while but it still is not
+        # plugged in
+        return now < self.inTheGarage + minutes(minutes=10)
+
+# 2. Car is connected but its state of charge is not up to date
+class CarStateOfCharge(threading.Thread):
+    def __init__(self, config):
+        super(CarStateOfCharge, self).__init__()
+        self.sensor = CarData(config)
+        self.pluggedIn = datetime(1970, 1, 1)
+
+    @test_loop('CarStateOfCharge',
+               'Car has been plugged in but the state of charge is outdated',
+               sleep=30)
+    def run(self):
+        if not ev_is_connected():
+            self.pluggedIn = datetime(1970, 1, 1)
+        elif not self.pluggedIn:
+            self.pluggedIn = datetime.now()
+        if datetime.now() < self.pluggedIn + timedelta(seconds=4 * 60):
+            return True
+        return self.sensor.read() and \
+            self.sensor.datetime >= self.pluggedIn - timedelta(seconds=10 * 60)
 
 def main():
-    config = configparser.ConfigParser()
-    config.read("home.ini")
+    config = init(os.path.splitext(__file__)[0] + ".log")
 
-    global mailer
-    mailer = MyEmail(config['Email'])
-    vue = MyVue2(config['Emporia'])
     global reader
-    reader = SensorReader(vue)
-    threads = [ threading.Thread(target=lambda: test(hvac_yellow,
-                                                     'air handler running alone'))
-                # threading.Thread(target=hvac_red)
-               ]
-    for t in threads:
-        t.start()
+    reader = SensorReader(config['Emporia'])
+    global ev
+    ev = MyWallBox(config['Wallbox'])
 
-    # IP address monitoring
+    threading.Thread(target=hvac_yellow).start()
+    threading.Thread(target=hvac_red).start()
+    PoolRanLongEnough(config['Pool'], 3 * 60).start()
+    PoolFilterIsClean(config['Pool']).start()
+    CarIsPluggedIn(config['CarData']).start()
+    CarStateOfCharge(config['CarData']).start()
+    ecobee = MyEcobee(config['Ecobee'])
+    threading.Thread(target=lambda: sensor_is_running(ecobee)).start()
+
+    debug("... is now ready to run")
     while True:
         time.sleep(60)
-        # # Power sensor
-        # # ------------
-        # # Sometimes the Emporia service becomes inaccessible.
-        # usage = test(lambda: emporia(vue), "Emporia is unreachable",
-        #              end_msg = "Emporia is back")
-
-        # # HVAC
-        # # ----
-        # # 1. When the Yellow wire is shunt by float T-switch the air handler
-        # #    stops running but the Heat Pump is still running
-        # test(lambda: usage['A/C'] > .1 and usage['air handler'] < .1,
-        #      "air handler is not running while A/C is")
-        # # 2. When the Red wire is shunt by float T-switch, the air
-        # #    handler keeps running but the condensation and the heat
-        # #    pump do not run
-        # test(lambda: usage['A/C'] < .1 and usage['air handler'] > .1,
-        #      "air handler is not running while A/C is")
-        # print("HVAC is okay")
-
-        # # Pool
-        # # ----
-        # # 1. Out of range power consumption (June 21 2021: pool filter
-        # #    head cracked)
-        # # 2. Ran for a very short period of time (less than 2 minutes)
-        # #    July 7 2021: less than 2 minutes: pool pump capacitor
-        # #    died)
-        # time.sleep(60)
 
 if __name__ == "__main__":
     main()
